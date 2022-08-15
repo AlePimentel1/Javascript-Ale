2ffdf3b2aeb63c043a23be05550fe60df7934c02
Alessandro Pimentel <pimentelaleee@gmail.com>
2022-08-15 19:11:01 -0300

    Cambios

--- js/index.js
+++ js/index.js
@@ -2,18 +2,15 @@ const bienvenida = alert(
   "Llene los campos y luego envie el formulario (Recibira sus datos en consola)"
 );
 const camposDeTexto = document.querySelectorAll("input");
-const captadorBtn = document.querySelector(".btn.btn-info");
+const captadorBtn = document.querySelector("#btnEnviarDatos");
+
+captadorBtn.addEventListener("click", () => {
+  console.log("Se ha hecho click sobre el boton");
+  captarTextoDeCampo();
+});
 
 function captarTextoDeCampo() {
   camposDeTexto.forEach((e) => {
     console.log(e.value);
   });
 }
-
-function enviarInput() {
-  captadorBtn.addEventListener("click", () => {
-    console.log("Se ha hecho click sobre el boton");
-  });
-}
-
-enviarInput();
